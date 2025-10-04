515ea54a866f6720db1acde6eab21c29990df121
comittonxxa2 <sakura.kinomoto.tomoyo.daidouji@gmail.com>
2025-10-04 15:55:12 +0900

    Add files via upload

--- app/src/main/java/src/comitton/imageview/MyImageView.java
+++ app/src/main/java/src/comitton/imageview/MyImageView.java
@@ -2,7 +2,6 @@ package src.comitton.imageview;
 
 import src.comitton.common.DEF;
 import src.comitton.common.Logcat;
-import src.comitton.config.SetImageActivity;
 import src.comitton.fileview.view.DrawNoticeListener;
 import src.comitton.jni.CallImgLibrary;
 import src.comitton.common.GuideView;
@@ -10,7 +9,6 @@ import src.comitton.common.GuideView.UpdateListener;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
@@ -31,8 +29,6 @@ import android.os.SystemClock;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
-import androidx.preference.PreferenceManager;
 
 public class MyImageView extends SurfaceView implements SurfaceHolder.Callback, UpdateListener, DrawNoticeListener, Callback, Runnable {
 	private static final String TAG = "MyImageView";
@@ -147,7 +143,7 @@ public class MyImageView extends SurfaceView implements SurfaceHolder.Callback, 
 	private int mCurrentPage = 0;
 	private boolean mPageLock = false;
 
-	private static int mThreadWaitLoop = 0;
+	private int mThreadWaitLoop = 0;
 
 	// ルーペ表示
 	private int mZoomMode = ZOOM_NONE;
@@ -168,8 +164,6 @@ public class MyImageView extends SurfaceView implements SurfaceHolder.Callback, 
 
 	private boolean mViewTapSw = false;
 
-	private static SharedPreferences mSharedPreferences;
-
 	public MyImageView(Activity activity) {
 		super(activity);
 		mActivity = activity;
@@ -179,8 +173,6 @@ public class MyImageView extends SurfaceView implements SurfaceHolder.Callback, 
 		mWorkRect = new Rect();
 		mHandler = new Handler(this);
 		new TouchPanelView(activity, 1);
-
-		mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(activity);
 
 		requestFocus();
 		mHolder = getHolder();
@@ -266,8 +258,8 @@ public class MyImageView extends SurfaceView implements SurfaceHolder.Callback, 
 		updateNotify();
 	}
 
-	private static boolean mDrawLock;
-	public static void lockDraw() {
+	private boolean mDrawLock;
+	public void lockDraw() {
 		mThreadWaitLoop = 0;
 		mDrawLock = true;
 	}
@@ -1045,9 +1037,6 @@ public class MyImageView extends SurfaceView implements SurfaceHolder.Callback, 
 				// タップ操作の設定を表示
 				TouchPanelView.SetViewArea(mDispWidth, mDispHeight);
 				TouchPanelView.Drawmain(canvas);
-				if (SetImageActivity.getViewPause(mSharedPreferences)) {
-					lockDraw();
-				}
 			}
 		}
 	}
