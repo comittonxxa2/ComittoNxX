002a442b9ebef4532a8bd4afc6385d6f44e38713
comittonxxa2 <sakura.kinomoto.tomoyo.daidouji@gmail.com>
2026-07-11 21:09:30 +0900

    Add files via upload

--- app/src/main/java/src/comitton/helpview/HelpActivity.java
+++ app/src/main/java/src/comitton/helpview/HelpActivity.java
@@ -1,15 +1,11 @@
 package src.comitton.helpview;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
-import src.comitton.common.MultiProcessPreferences;
 import src.comitton.config.SetCommonActivity;
 import src.comitton.cropimageview.CropImageActivity;
 import src.comitton.fileview.FileSelectActivity;
@@ -26,37 +22,11 @@ public class HelpActivity extends AppCompatActivity {
 	private final int mSdkVersion = android.os.Build.VERSION.SDK_INT;
 	private static SharedPreferences sharedPreferences;
 
-	@Override
-	protected void attachBaseContext(Context newBase) {
-		// 共通のSharedPreferencesから現在のテーマ設定を取得
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(newBase);
-		int themeValue = SetCommonActivity.getSelectTheme(sp);
-		Configuration overrideConfig = new Configuration(newBase.getResources().getConfiguration());
-		// 各モードに合わせてuiModeを強制上書きする
-		if (themeValue == 0) {
-			overrideConfig.uiMode = (overrideConfig.uiMode & ~Configuration.UI_MODE_NIGHT_MASK) | Configuration.UI_MODE_NIGHT_NO;
-		}
-		else if (themeValue == 1) {
-			overrideConfig.uiMode = (overrideConfig.uiMode & ~Configuration.UI_MODE_NIGHT_MASK) | Configuration.UI_MODE_NIGHT_YES;
-		}
-		// 上書きした環境変数を適用
-		Context context = newBase.createConfigurationContext(overrideConfig);
-		super.attachBaseContext(context);
-	}
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-			try {
-				WebView.setDataDirectorySuffix("webview_process");
-			}
-			catch (IllegalStateException e) {
-				e.printStackTrace();
-			}
-		}
-		sharedPreferences = MultiProcessPreferences.getInstance(this);
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
 		mNotice = SetCommonActivity.getForceHideStatusBar(sharedPreferences);
 		if (mNotice) {
@@ -94,7 +64,7 @@ public class HelpActivity extends AppCompatActivity {
         String url = intent.getStringExtra("Url");
 
         mWebView = (WebView) new WebView(this);
-		SharedPreferences sharedPreferences = MultiProcessPreferences.getInstance(this);
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 		FileSelectActivity.applyAppTheme(sharedPreferences);
         mWebView.loadUrl("file:///android_asset/" + url);
         setContentView(mWebView);
